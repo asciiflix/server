2dc77f8de38aaab3a06afbeb0d01c6d34780442a
Lukas <lukas@leucali.net>
2021-06-10 21:38:04 +0200

    Changed Config for MongoDB & added Logrus

--- database/mongo.go
+++ database/mongo.go
@@ -3,9 +3,10 @@ package database
 import (
 	"context"
 	"fmt"
-	"os"
+	"net/url"
 	"time"
 
+	"github.com/asciiflix/server/config"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -22,14 +23,26 @@ var Global_mongo_context context.Context
 
 func ConnectToMongo() {
 	var err error
-	Global_mongo_client, err = mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+
+	dsn := url.URL{
+		User: url.UserPassword(config.MongoDB.User, config.MongoDB.Password),
+		Host: fmt.Sprintf("%s:%s", config.MongoDB.Host, config.MongoDB.Port),
+	}
+
+	//Create Client
+	Global_mongo_client, err = mongo.NewClient(options.Client().ApplyURI("mongodb:" + dsn.String()))
+
+	//Panic
 	panicWhenErr(err)
+
+	//Context
 	Global_mongo_context, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
 	err = Global_mongo_client.Connect(Global_mongo_context)
 	panicWhenErr(err)
 	// defer client.Disconnect(Global_mongo_context)
-	panicWhenErr(err)
-	fmt.Println("Connected to Mongo")
+	//panicWhenErr(err)
+	config.Log.Info("Connected to MongoDB")
 }
 
 func panicWhenErr(err error) {
