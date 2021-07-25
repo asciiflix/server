ceef5ee3813a718a2a25698bd798d9133c4327dc
Tim Horlacher <trimin.nurum@gmail.com>
2021-07-25 21:30:57 +0200

    Added no smtp option

--- utils/mail.go
+++ utils/mail.go
@@ -10,14 +10,19 @@ import (
 )
 
 func SendWelcomeMail(to string, username string, code string) error {
-
+	//Return if no server is configured
+	if config.SMTP.Host == "" {
+		return nil
+	}
 	//Authenticate with server using login and password
 	auth := smtp.PlainAuth("", config.SMTP.User, config.SMTP.Password, config.SMTP.Host)
 
+	//Parse template
 	mail, _ := template.ParseFiles("./templates/welcome-mail.html")
 
 	var body bytes.Buffer
 
+	//Set MIME Header
 	mimeHeaders := "MIME-version: 1.0; \nContent-Type: text/html; charset=\"UTF-8\";\n\n"
 	body.Write([]byte(fmt.Sprintf("From: %s\r\nTo: %s\r\nSubject: Welcome to ASCIIflix\r\n%s\n\n", config.SMTP.User, to, mimeHeaders)))
 
@@ -33,7 +38,7 @@ func SendWelcomeMail(to string, username string, code string) error {
 		URL_Verify: config.ApiConfig.FrontendURL + "/verify",
 	})
 
-	//SendMail
+	//Send Mail
 	err := smtp.SendMail(config.SMTP.Host+":"+config.SMTP.Port, auth, config.SMTP.User, []string{to}, body.Bytes())
 	return err
 }
